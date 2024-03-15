2f7de6b627f58a746b1de9f479be5d824bc2e067
dijahmohammed <151194271+dijahmohammed@users.noreply.github.com>
2024-03-15 19:23:24 +0000

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./{index.html}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    // Other PostCSS plugins can be added here
-  ]
 }
