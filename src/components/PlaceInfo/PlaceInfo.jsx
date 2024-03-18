33de0310627c947a658f20f4d908e9235b1338fb
Nana <frimpongnana8@gmail.com>
2024-03-18 09:10:35 +0000

    added code that handles the submit and the input

--- src/components/PlaceInfo/PlaceInfo.jsx
+++ src/components/PlaceInfo/PlaceInfo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import SearchForm from './SearchForm'
 import { useState } from 'react'
-import TravelAdvisorApi from '../../utils/TravelAdvisorApi'
-import Info from './Info'
-import Hotels from './Hotels'
-import Attraction from '../../utils/Attraction'
-import Attractions from './Attractions'
+// import TravelAdvisorApi from '../../utils/TravelAdvisorApi'
+// import Info from './Info'
+// import Hotels from './Hotels'
+// import Attraction from '../../utils/Attraction'
+// import Attractions from './Attractions'
 
 function PlaceInfo() {
 
@@ -14,8 +14,29 @@ const [searchData, setSearchData] = useState({
     results: [],
     hotel: [],
     attraction: [],
+
 })
 
+
+function handleInputChange(e){
+
+    let value = e.target.value 
+  
+    setSearchData({
+      ...searchData,
+      search: value
+    })
+  
+  }
+  
+  function handleFormSubmit(event){
+    event.preventDefault()
+  
+    searchLocation(searchData.search)
+  
+   
+  
+  }
 
 
 
@@ -25,7 +46,18 @@ const [searchData, setSearchData] = useState({
 
 
   return (
-    <div>PlaceInfo</div>
+    <>
+
+    <h3>Search location</h3>
+    <SearchForm 
+   value ={searchData.search}
+   handleFormSubmit={handleFormSubmit}
+   handleInputChange={handleInputChange}
+    />
+    
+    
+    
+    </>
   )
 }
 
