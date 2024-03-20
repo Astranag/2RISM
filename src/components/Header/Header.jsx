42bb6bcb74bd6668b2aa3125956ac9708e1fb0fd
Stivin <stivinmavelil@gmail.com>
2024-03-20 20:42:47 +0100

    Update the project postcss configuration.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Travel planner</a>
+    <a className="navbar-brand" href="#">2RISM</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
