2f681c39cd3d80fa2ab477199f5e12d21c01d074
c-woolrych <c.woolrych@gmail.com>
2024-03-20 17:23:09 +0000

    add styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Travel from '../utils/TravelAdvisorApi'
 import {NavLink, Link} from 'react-router-dom'
 import logo from '../../assets/logo.png'
+import './header.css'
 
 
 
@@ -11,15 +12,15 @@ function Header() {
 
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+    <nav className="navbar nav navbar-expand-lg bg-warning">
+  <div className="container-fluid" >
     <a className="navbar-brand" href="#"><img src={logo} width="100" height="100" alt="2RISM"/></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div style={{ marginTop: '10px', listStyle: 'none', display: 'flex' }}> 
+    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+    </button> */}
+    <div className="ms-auto nav-tabs" style={{ marginTop: '10px', listStyle: 'none', display: 'flex'}}> 
   <li style={{ marginRight: '10px' }}> 
-    <NavLink className="nav-link active" activeClassName='active' exact to='/'>Home</NavLink>
+    <NavLink className="nav-link" activeClassName='active' exact to='/'>Home</NavLink>
   </li>
   <li style={{ marginRight: '10px' }}> 
     <NavLink className="nav-link" activeClassName='active' exact to='/map'>Map</NavLink>
@@ -29,7 +30,7 @@ function Header() {
   </li>
 </div>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <NavLink className="nav-link active" activeClassName='active' exact to='/'>Home</NavLink>
@@ -40,20 +41,8 @@ function Header() {
         <li className="nav-item">
           <NavLink className="nav-link" activeClassName='active' exact to='/extra'>Extra</NavLink>
         </li>
-       
-        
-        
-      
-      </ul>
-      <form className="d-flex" role="search" >
-        <input className="form-control me-2"
-         type="search" 
-         placeholder="Search" 
-         aria-label="Search"
-         />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+      </ul> */}
+    {/* </div> */}
   </div>
 </nav>
   )
