70cb9aecd2eb6137173ce9427156258d0ba8b6b5
c-woolrych <c.woolrych@gmail.com>
2024-03-21 18:57:49 +0000

    change header styling for extra info

--- src/components/ExtraInfo/ExtraInfo.jsx
+++ src/components/ExtraInfo/ExtraInfo.jsx
@@ -63,7 +63,7 @@ function ExtraInfo() {
 
     return (
         <>
-         <h1 style={{ fontSize: '5vw', color: '#333', textAlign: 'center', marginBottom: '20px' }}> Explore detailed information about a country</h1>
+         <h1 style={{ color: '#333', textAlign: 'center', marginBottom: '20px' }}> Explore detailed information about a country</h1>
             <SearchBar
                 value={searchData.search}
                 submit={submit}
